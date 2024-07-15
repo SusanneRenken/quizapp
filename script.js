9c34cc0b0fcb2c53347ba5a194741247914476ba
SusanneRenken <renken.susanne@gmail.com>
2024-07-15 20:31:49 +0200

    btn next question

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let currentQuestion = 0;
 
 function init() {
-  let allQuestions = document.getElementById("all_questions");
-  allQuestions.innerHTML = questions.length;
+  document.getElementById("all_questions").innerHTML = questions.length;
   showQuestion();
 }
 
@@ -12,17 +11,36 @@ function showQuestion() {
   for (let i = 1; i < 5; i++) {
     document.getElementById(`answer_${i}`).innerHTML = question[`answer_${i}`];
   }
+  document.getElementById("current-question").innerHTML = currentQuestion + 1;
 }
 
-function answer(selection){
-    let question = questions[currentQuestion];
-    let rightAnswer = `answer_${question[`right_answer`]}`;
+function answer(selection) {
+  let question = questions[currentQuestion];
+  let rightAnswer = `answer_${question[`right_answer`]}`;
 
-    if(rightAnswer == selection){
-        // document.getElementById(selection).classList.add('green');
-        alert("RICHTIG")
-    }else{
-        // document.getElementById(selection).classList.add('red');
-        alert("FALSCH")
-    }
-};
+  if (rightAnswer == selection) {
+    document
+      .getElementById(selection)
+      .parentNode.classList.add("bg-success-subtle");
+  } else {
+    document
+      .getElementById(selection)
+      .parentNode.classList.add("bg-danger-subtle");
+    document
+      .getElementById(rightAnswer)
+      .parentNode.classList.add("bg-success-subtle");
+  }
+  document.getElementById("next_button").disabled = false;
+}
+
+function nextQuestion() {
+  for (let i = 1; i < 5; i++) {
+    let answerContainer = document.getElementById(`answer_${i}`);
+    answerContainer.parentNode.classList.remove("bg-danger-subtle");
+    answerContainer.parentNode.classList.remove("bg-success-subtle");
+  }
+
+  document.getElementById("next_button").disabled = true;
+  currentQuestion++;
+  init();
+}
